b8fbc58211d094da7a776f2689ac03556da7787e
lukyuxuan0928 <luke.yuxuan.0928@gmail.com>
2017-11-17 19:14:01 -0800

    Added resource student and leaves

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :student
+  resources :leaves
   root to: "welcome#show"
 end
